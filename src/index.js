ee649fdde7d7a5d1975d1611466244f59c77fcf9
Terje Sakariassen <terje@sakariassen.guru>
2018-08-27 13:03:45 +0200

    refactored index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTsearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCZ8ZtDkjPHmI8vpuVW1NuHdqxqLimPtRs';
 
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+class App extends Component {
+constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTsearch({key: API_KEY, term: 'dogs'}, (videos) => {
+        this.setState({ videos })
+    });
 }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'))
